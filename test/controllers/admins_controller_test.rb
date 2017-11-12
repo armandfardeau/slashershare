d0ff911dbc3bf05e1d9ea4e2287683d45307808d
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-12 21:26:56 +0100

    add test for active admin

---
+++ test/controllers/admins_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class AdminsControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+  
+  # Admin redirection test
+
+  test "should get redirect if not logged in" do
+    get admin_root_path
+    assert_response :redirect
+    assert_redirected_to admin_user_session_path
+  end
+
+end
