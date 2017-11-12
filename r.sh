1c2a5513e4ff9c36dc3f8a036ca1b901c1944861
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-12 16:43:50 +0100

    improve installer

--- r.sh
+++ r.sh
@@ -3,7 +3,8 @@ curl -sSL https://get.rvm.io | bash -s stable --ruby
 rvm install ruby-2.4.2
 rvm use 2.4.2
 bundle install
-rails reload
 spring stop
+rails db:environment:set RAILS_ENV=development
+rails reload
 open http://localhost:3000
 rails sg
