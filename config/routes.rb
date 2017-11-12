ef912ad1cfdf482b217c64a59d48b2143a5ab707
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-12 03:08:36 +0100

    add auth prefix to devise route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users, only: [:show]
 
   devise_for :users,
-             :path => '',
+             :path => 'auth',
              :path_names => {
                  :edit => 'profile'
              }
