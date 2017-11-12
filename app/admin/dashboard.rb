dde9c635544a2bdb852fb81e5c89bff999bbe142
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-12 20:55:20 +0100

    update dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,33 +1,38 @@
-ActiveAdmin.register_page "Dashboard" do
+ActiveAdmin.register_page 'Dashboard' do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu priority: 1, label: proc {I18n.t('active_admin.dashboard')}
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: proc {I18n.t('active_admin.dashboard')} do
+    columns do
+      column do
+        panel 'Recent Users' do
+          ul do
+            User.last(5).map do |user|
+              li link_to(user.username, admin_users_path(user))
+            end
+          end
+        end
+      end
+      column do
+        panel 'Recent Users' do
+          ul do
+            User.last(5).map do |user|
+              li link_to(user.username, admin_users_path(user))
+            end
+          end
+        end
       end
     end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    columns do
+      column do
+        div do
+          br
+          text_node %{<iframe src='https://rpm.newrelic.com/public/charts/6VooNO2hKWB'
+                              width='500' height='300' scrolling='no' frameborder='no'>
+                      </iframe>}.html_safe
+        end
+      end
+    end
+  end
 end
