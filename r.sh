381d56fa12360c2ecb07bdf8bbf422754415141d
Armand Fardeau <armandfardeau@users.noreply.github.com>
2017-11-13 10:40:17 +0100

    update r.sh with bundler install

--- r.sh
+++ r.sh
@@ -2,6 +2,7 @@
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 rvm install ruby-2.4.2
 rvm use 2.4.2
+gem install bundler
 bundle install
 spring stop
 rails db:environment:set RAILS_ENV=development
