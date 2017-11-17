2e9e784d0ee0f3339a5399c1442bb93439c54b5a
Armand Fardeau <armandfardeau@users.noreply.github.com>
2017-11-17 15:44:12 +0100

    Refactoring with helpers (#6)
    * better render of partials
    
    * refactor nav
    
    * fix typo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nice_display_of_user_options(user)
+    unless user_signed_in?
+       render 'layouts/partials/sign_in_up'
+    else
+      render'layouts/partials/profile_options', locals: {current_user: user}
+     end
+  end
+
 end
