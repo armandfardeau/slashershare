ec1cf3c2615fa29fcecf30ad7bba3ea95555dccb
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-19 23:02:42 +0100

    fix typ in seeds files

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,6 +1,6 @@
 admin_email = ENV['ADMIN_EMAIL']
 admin_password = ENV['ADMIN_PASSWORD']
 
-if Rails.env.production
+if Rails.env.production?
   AdminUser.create!(email: admin_email, password: admin_password, password_confirmation: admin_password)
 end
