fb1d29c89a1cf79c37b42f81ac048aa1e39bc547
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-21 21:22:48 +0100

    fix style issue in models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   has_many :offers, dependent: :destroy
   acts_as_taggable
-  has_attached_file :avatar, :styles => {:medium => "300x300>", :thumb => "100x100>"},
-                    :default_url => '/assets/default_profil_picture.svg'
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :avatar, styles: {medium: '300x300>', thumb: '100x100>'},
+                    default_url: '/assets/default_profil_picture.svg'
+  validates_attachment_content_type :avatar, content_type: %r{/\Aimage\/.*\Z/}
 end
