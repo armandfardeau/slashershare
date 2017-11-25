1c35c41b0519a3118b53ef6881a19f3cf434a847
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-25 12:25:03 +0100

    add enterprise to user

--- db/migrate/20171124083900_add_enterprise_to_user.rb
+++ db/migrate/20171124083900_add_enterprise_to_user.rb
@@ -1,5 +1,5 @@
 class AddEnterpriseToUser < ActiveRecord::Migration[5.1]
   def change
-    add_column :users, :enterprise, :boolean
+    add_column :users, :enterprise, :boolean, default: false
   end
 end
