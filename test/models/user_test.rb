85ca89d9f28417bd055e6ee2d57fa0947b3ed929
Armand Fardeau <fardeauarmand@gmail.com>
2017-11-25 12:28:14 +0100

    add enterprise test for user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,6 +55,36 @@ class UserTest < ActiveSupport::TestCase
     assert u.valid?
   end
 
+  test 'enterprise should be true if new record with true' do
+    u = User.create(
+        email: 'userOne@email.com',
+        password: 'password',
+        enterprise: true
+    )
+    assert u.valid?
+    assert_equal u.enterprise, true
+  end
+
+  test 'enterprise should be false if new record' do
+    u = User.create(
+        email: 'userOne@email.com',
+        password: 'password'
+    )
+    assert u.valid?
+    assert_equal u.enterprise, false
+  end
+
+  test 'enterprise should be true if not new record' do
+    u = User.create(
+        email: 'userOne@email.com',
+        password: 'password'
+    )
+    u.enterprise = true
+    u.save
+    assert u.valid?
+    assert_equal u.enterprise, true
+  end
+
   test 'is valid with a username' do
     u = User.create(
         username: 'userTest',
@@ -71,13 +101,13 @@ class UserTest < ActiveSupport::TestCase
         password: 'password'
     )
 
-    c = User.create(
+    u = User.create(
         username: 'userTest',
         email: 'userOne@email.com',
         password: 'password'
     )
-    assert c.errors[:email].any?
-    refute c.valid?
+    assert u.errors[:email].any?
+    refute u.valid?
   end
 
   test 'is not valid with incorrect attachment' do
