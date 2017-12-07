f813754076188b8f9e7a2ceaafccc3887ed9b1d0
Armand Fardeau <armandfardeau@users.noreply.github.com>
2017-12-07 15:38:53 +0100

    Add mailer (#34)
    * add .env.example
    
    * add .env.example
    
    * update schema
    
    * fix issue with create action
    
    * update project dependancies
    
    * add config for mailer in dev env
    
    * Add local url to env var
    
    * add email to notify user when they add a new offer
    
    * fix style offense
    
    * Add sendgrid as SMTP server
    
    * fix typo in mailer
    
    * mode send grid config to production

--- db/schema.rb
+++ db/schema.rb
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20171124083900) do
     t.string "avatar_content_type"
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.boolean "enterprise"
+    t.boolean "enterprise", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
