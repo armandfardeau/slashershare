badd663df7d36d65986e488b53467a72122d66fa
Armand Fardeau <fardeauarmand@gmail.com>
2017-12-07 14:11:36 +0100

    add config for mailer in dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,9 @@ Rails.application.configure do
       moderation: 'aws_rek'
   )
 
-  config.action_mailer.default_url_options = {host: 'localhost', port: 3000}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {address: '127.0.0.1', port: 1025}
+  config.action_mailer.raise_delivery_errors = false
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
